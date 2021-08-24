f190ad64d7fb64e8f3af61738384d99e97826eca
Grigorii Merkushev <brushknight@pop-os.localdomain>
2021-08-24 23:36:56 +0200

    fix: overlay

--- app/src/components/menu/Menu.jsx
+++ app/src/components/menu/Menu.jsx
@@ -27,6 +27,20 @@ const Item = ({title, onEdit}) => {
 	)
 }
 
+const AddButton = ({t, onClick}) => {
+	return (
+		<button className={'list-navigation__title-button'} onClick={onClick}>
+			<svg className={'list-navigation__title-button-svg'} data-icon="plus" width="16" height="16"
+				 viewBox="0 0 16 16">
+				<path className={'list-navigation__title-button-path'}
+					  d="M13 7H9V3c0-.55-.45-1-1-1s-1 .45-1 1v4H3c-.55 0-1 .45-1 1s.45 1 1 1h4v4c0 .55.45 1 1 1s1-.45 1-1V9h4c.55 0 1-.45 1-1s-.45-1-1-1z"
+					  fillRule="evenodd"></path>
+			</svg>
+			<span className={'list-navigation__title-button-text'}>{t('global.button_add')}</span>
+		</button>
+	)
+}
+
 const Menu = ({ isOpen, setIsOpen, lists, categories, fetchLists, fetchCategories, t }) => {
 	const history = useHistory()
 
@@ -40,19 +54,25 @@ const Menu = ({ isOpen, setIsOpen, lists, categories, fetchLists, fetchCategorie
 		toggleStyle = ''
 	}
 
+	const createCategory = () => {
+		history.push('/category-new')
+	}
+
+	const createList = () => {
+		history.push('/list-new')
+	}
+
 	return (
 		<div className={'list-navigation__wrapper-for-list ' + toggleStyle}>
 			<div className={'list-navigation__wrapper-for-title'}>
 				<h2 className={'list-navigation__title-list'}>Списки</h2>
-				<button className={'list-navigation__title-button'}>
-					<svg className={'list-navigation__title-button-svg'} data-icon="plus" width="16" height="16"
-						viewBox="0 0 16 16">
-						<path className={'list-navigation__title-button-path'}
-							d="M13 7H9V3c0-.55-.45-1-1-1s-1 .45-1 1v4H3c-.55 0-1 .45-1 1s.45 1 1 1h4v4c0 .55.45 1 1 1s1-.45 1-1V9h4c.55 0 1-.45 1-1s-.45-1-1-1z"
-							fillRule="evenodd"></path>
-					</svg>
-					<span className={'list-navigation__title-button-text'}>{t('global.button_add')}</span>
-				</button>
+				<AddButton
+					t={t}
+					onClick={() => {
+						setIsOpen(false)
+						createList()
+					}}
+				/>
 			</div>
 			<ul className={'list-navigation__list'}>
 				{lists.items.map(item => (
@@ -67,15 +87,13 @@ const Menu = ({ isOpen, setIsOpen, lists, categories, fetchLists, fetchCategorie
 			</ul>
 			<div className={'list-navigation__wrapper-for-title'}>
 				<h2 className={'list-navigation__title-list'}>Категории</h2>
-				<button className={'list-navigation__title-button'}>
-					<svg className={'list-navigation__title-button-svg'} data-icon="plus" width="16" height="16"
-						 viewBox="0 0 16 16">
-						<path className={'list-navigation__title-button-path'}
-							  d="M13 7H9V3c0-.55-.45-1-1-1s-1 .45-1 1v4H3c-.55 0-1 .45-1 1s.45 1 1 1h4v4c0 .55.45 1 1 1s1-.45 1-1V9h4c.55 0 1-.45 1-1s-.45-1-1-1z"
-							  fillRule="evenodd"></path>
-					</svg>
-					<span className={'list-navigation__title-button-text'}>{t('global.button_add')}</span>
-				</button>
+				<AddButton
+					t={t}
+					onClick={() => {
+						setIsOpen(false)
+						createCategory()
+					}}
+				/>
 			</div>
 			<ul className={'list-navigation__list'}>
 				{categories.items.map(item => (
